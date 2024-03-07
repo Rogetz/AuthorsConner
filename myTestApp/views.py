54bdf2deed534ba31d01c7024f3284a9eafb1179
Rogetz <ronnyogetaz@gmail.com>
2024-03-07 10:24:21 +0300

    some djangoUpdates

--- myTestApp/views.py
+++ myTestApp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from myTestApp.models import MyBookModel
 
-# Create your views here.
+"""def index(request):
+    return HttpResponse("Hello world!")"""
+
+def index(request):
+    # uses the defined templates section to find the templates so it's not much of a problem for it.
+    books = MyBookModel.objects.all().values()
+    output = "the books:\n"
+    output_long = "the long books:\n "
+    for book in books:
+        output += book['title']
+        output_long += str(book)
+    return render(request,"test.html",{'output':output,'output_long':output_long})
--- secondTest/urls.py
+++ secondTest/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,re_path
+from django.urls import path,re_path,include
 from secondTest.views import hour_offset,current_datetime,name_display
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     #path('admin/', admin.site.urls),
     #(r'^now/$', current_datetime),
+    path("test/",include("myTestApp.urls")),
     path("admin",admin.site.urls),
     path('now/',current_datetime),
     #(r'^now/(plus|minus)(\d{1,2})hours/$', hour_offset),
